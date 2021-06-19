f4fa4c16b8db3f87292303672a2c297500f0d0ad
Vetrano89 <crvetrano@gmail.com>
2021-06-19 14:29:05 -0400

    Remove console logs from step-helpers

--- src/helpers/step-helpers.ts
+++ src/helpers/step-helpers.ts
@@ -6,15 +6,11 @@ export function isInitialStep(currentStep: MainStep, subStepIndex: number) {
 
 export function getStepProgress(currentStep: MainStep, subStepIndex: number) {
   const currentStepCompletedSubSteps = subStepIndex + 1;
-  console.log(currentStepCompletedSubSteps);
   const totalCompletedSubSteps: number = mainSteps
     .filter((step) => step.stepNumber < currentStep.stepNumber)
     .reduce((acc, step) => {
       return acc = acc + step.subSteps.length
     }, 0) + currentStepCompletedSubSteps;
-
-  console.log(totalCompletedSubSteps);
-  console.log(TOTAL_STEPS);
 
   return totalCompletedSubSteps / TOTAL_STEPS * 100;
 }
